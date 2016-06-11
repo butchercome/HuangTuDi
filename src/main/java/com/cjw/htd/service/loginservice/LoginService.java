4df8cf92e677b8bf78dbb447d272b4dc4d0d5852
Administrator <836709001@qq.com>
2016-06-11 23:27:10 +0800

    spring mvc   尝试

--- src/main/java/com/cjw/htd/service/loginservice/LoginService.java
+++ src/main/java/com/cjw/htd/service/loginservice/LoginService.java
@@ -1,8 +1,11 @@
 package com.cjw.htd.service.loginservice;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Administrator on 2016/6/6.
  */
+@Service
 public class LoginService {
 
 
